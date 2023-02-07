ef016f67a211eb74c0f30755fd12c8a001f53784
张扬 <bluesyoung_web@163.com>
2023-02-07 08:44:27 +0800

    feat: :sparkles: change about api request to only server

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,18 +1,24 @@
 /*
  * @Author: zhangyang
  * @Date: 2023-01-16 15:51:30
- * @LastEditTime: 2023-01-16 15:51:31
+ * @LastEditTime: 2023-02-01 16:25:20
  * @Description: 
  */
 import { For, Accessor, createResource } from "solid-js";
 import { useRouteData } from "solid-start";
+import { createServerData$ } from "solid-start/server";
 
 type Student = { name: string; house: string; }
 
 export function routeData() {
-  const [students] = createResource(async () => {
-    const response = await fetch("https://hogwarts.deno.dev/students");
-    return await response.json() as Student[];
+  const students = createServerData$(async (...args) => {
+    try {
+      const response = await fetch("https://hogwarts.deno.dev/students");
+      return await response.json() as Student[];
+    } catch (error) {
+      console.error(error);
+    }
+    return [];
   });
 
   return { students };
